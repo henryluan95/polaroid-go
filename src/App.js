2a2af8c6283aa1b8a8d07c402e4a7aa13f4440ef
Henry Luan <henryluan95@gmail.com>
2023-01-27 19:47:15 -0500

    render 3D

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Spline from "@splinetool/react-spline";
+import "./App.scss";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Spline
+      className="spline"
+      scene="https://prod.spline.design/ruO4eHIG9FJ3UzMG/scene.splinecode"
+    />
   );
 }
-
-export default App;
